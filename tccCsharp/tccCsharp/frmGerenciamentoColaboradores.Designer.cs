ff0773758f07ece538dda8d1b759a578adbd1873
AlineTiemiAwoki <aline-tiemi.awoki@unesp.br>
2023-09-05 15:30:50 -0300

    Design Gerenciamento de Colaboradores
    inicio do design

---
+++ tccCsharp/tccCsharp/frmGerenciamentoColaboradores.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace tccCsharp
+{
+    partial class frmGerenciamentoColaboradores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpBase1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpBase2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpBase4 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpBase3 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblNomeP = new System.Windows.Forms.Label();
+            this.lblNomeAutores = new System.Windows.Forms.Label();
+            this.lblCompartilharC = new System.Windows.Forms.Label();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.lblCompartilhar = new System.Windows.Forms.Label();
+            this.lblAcesso = new System.Windows.Forms.Label();
+            this.radSim = new System.Windows.Forms.RadioButton();
+            this.tlpBase5 = new System.Windows.Forms.TableLayoutPanel();
+            this.radNao = new System.Windows.Forms.RadioButton();
+            this.rgbMensagem = new tccCsharp.RoundedGroupBox();
+            this.rgbNomeP = new tccCsharp.RoundedGroupBox();
+            this.rgbNomeAutores = new tccCsharp.RoundedGroupBox();
+            this.rgbCompartilharCom = new tccCsharp.RoundedGroupBox();
+            this.rgbAcesso = new tccCsharp.RoundedGroupBox();
+            this.tlpBase1.SuspendLayout();
+            this.tlpBase2.SuspendLayout();
+            this.tlpBase4.SuspendLayout();
+            this.tlpBase3.SuspendLayout();
+            this.tlpBase5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpBase1
+            // 
+            this.tlpBase1.ColumnCount = 1;
+            this.tlpBase1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpBase1.Controls.Add(this.tlpBase2, 0, 1);
+            this.tlpBase1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpBase1.Location = new System.Drawing.Point(0, 0);
+            this.tlpBase1.Name = "tlpBase1";
+            this.tlpBase1.RowCount = 2;
+            this.tlpBase1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.5485F));
+            this.tlpBase1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.45151F));
+            this.tlpBase1.Size = new System.Drawing.Size(1198, 598);
+            this.tlpBase1.TabIndex = 0;
+            // 
+            // tlpBase2
+            // 
+            this.tlpBase2.ColumnCount = 2;
+            this.tlpBase2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.07718F));
+            this.tlpBase2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.92282F));
+            this.tlpBase2.Controls.Add(this.tlpBase4, 1, 0);
+            this.tlpBase2.Controls.Add(this.tlpBase3, 0, 0);
+            this.tlpBase2.Location = new System.Drawing.Point(3, 90);
+            this.tlpBase2.Name = "tlpBase2";
+            this.tlpBase2.RowCount = 1;
+            this.tlpBase2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpBase2.Size = new System.Drawing.Size(1192, 505);
+            this.tlpBase2.TabIndex = 0;
+            // 
+            // tlpBase4
+            // 
+            this.tlpBase4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpBase4.ColumnCount = 1;
+            this.tlpBase4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpBase4.Controls.Add(this.lblAcesso, 0, 0);
+            this.tlpBase4.Controls.Add(this.rgbAcesso, 0, 1);
+            this.tlpBase4.Location = new System.Drawing.Point(611, 3);
+            this.tlpBase4.Name = "tlpBase4";
+            this.tlpBase4.RowCount = 3;
+            this.tlpBase4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.432433F));
+            this.tlpBase4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.56757F));
+            this.tlpBase4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 202F));
+            this.tlpBase4.Size = new System.Drawing.Size(578, 499);
+            this.tlpBase4.TabIndex = 0;
+            // 
+            // tlpBase3
+            // 
+            this.tlpBase3.ColumnCount = 2;
+            this.tlpBase3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpBase3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 454F));
+            this.tlpBase3.Controls.Add(this.lblCompartilhar, 0, 9);
+            this.tlpBase3.Controls.Add(this.tlpBase5, 1, 9);
+            this.tlpBase3.Controls.Add(this.lblMensagem, 0, 7);
+            this.tlpBase3.Controls.Add(this.rgbMensagem, 1, 7);
+            this.tlpBase3.Controls.Add(this.rgbCompartilharCom, 1, 5);
+            this.tlpBase3.Controls.Add(this.lblCompartilharC, 0, 5);
+            this.tlpBase3.Controls.Add(this.rgbNomeAutores, 1, 3);
+            this.tlpBase3.Controls.Add(this.lblNomeAutores, 0, 3);
+            this.tlpBase3.Controls.Add(this.rgbNomeP, 1, 1);
+            this.tlpBase3.Controls.Add(this.lblNomeP, 0, 1);
+            this.tlpBase3.Location = new System.Drawing.Point(3, 3);
+            this.tlpBase3.Name = "tlpBase3";
+            this.tlpBase3.RowCount = 10;
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.98305F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.01695F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 68F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tlpBase3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.tlpBase3.Size = new System.Drawing.Size(579, 499);
+            this.tlpBase3.TabIndex = 1;
+            // 
+            // lblNomeP
+            // 
+            this.lblNomeP.AutoSize = true;
+            this.lblNomeP.Location = new System.Drawing.Point(3, 23);
+            this.lblNomeP.Name = "lblNomeP";
+            this.lblNomeP.Size = new System.Drawing.Size(89, 13);
+            this.lblNomeP.TabIndex = 0;
+            this.lblNomeP.Text = "Nome do Projeto:";
+            // 
+            // lblNomeAutores
+            // 
+            this.lblNomeAutores.AutoSize = true;
+            this.lblNomeAutores.Location = new System.Drawing.Point(3, 82);
+            this.lblNomeAutores.Name = "lblNomeAutores";
+            this.lblNomeAutores.Size = new System.Drawing.Size(97, 13);
+            this.lblNomeAutores.TabIndex = 1;
+            this.lblNomeAutores.Text = "Nome dos Autores:";
+            // 
+            // lblCompartilharC
+            // 
+            this.lblCompartilharC.AutoSize = true;
+            this.lblCompartilharC.Location = new System.Drawing.Point(3, 171);
+            this.lblCompartilharC.Name = "lblCompartilharC";
+            this.lblCompartilharC.Size = new System.Drawing.Size(91, 13);
+            this.lblCompartilharC.TabIndex = 2;
+            this.lblCompartilharC.Text = "Compartilhar com:";
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(3, 305);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(111, 13);
+            this.lblMensagem.TabIndex = 3;
+            this.lblMensagem.Text = "Mensagem (opcional):";
+            // 
+            // lblCompartilhar
+            // 
+            this.lblCompartilhar.AutoSize = true;
+            this.lblCompartilhar.Location = new System.Drawing.Point(3, 414);
+            this.lblCompartilhar.Name = "lblCompartilhar";
+            this.lblCompartilhar.Size = new System.Drawing.Size(107, 13);
+            this.lblCompartilhar.TabIndex = 4;
+            this.lblCompartilhar.Text = "Compartilhar Projeto?";
+            // 
+            // lblAcesso
+            // 
+            this.lblAcesso.AutoSize = true;
+            this.lblAcesso.Location = new System.Drawing.Point(3, 0);
+            this.lblAcesso.Name = "lblAcesso";
+            this.lblAcesso.Size = new System.Drawing.Size(110, 13);
+            this.lblAcesso.TabIndex = 1;
+            this.lblAcesso.Text = "Pessoas com acesso:";
+            // 
+            // radSim
+            // 
+            this.radSim.AutoSize = true;
+            this.radSim.Location = new System.Drawing.Point(112, 3);
+            this.radSim.Name = "radSim";
+            this.radSim.Size = new System.Drawing.Size(42, 17);
+            this.radSim.TabIndex = 2;
+            this.radSim.TabStop = true;
+            this.radSim.Text = "Sim";
+            this.radSim.UseVisualStyleBackColor = true;
+            // 
+            // tlpBase5
+            // 
+            this.tlpBase5.ColumnCount = 3;
+            this.tlpBase5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.09217F));
+            this.tlpBase5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.90783F));
+            this.tlpBase5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 175F));
+            this.tlpBase5.Controls.Add(this.radNao, 2, 0);
+            this.tlpBase5.Controls.Add(this.radSim, 1, 0);
+            this.tlpBase5.Location = new System.Drawing.Point(128, 417);
+            this.tlpBase5.Name = "tlpBase5";
+            this.tlpBase5.RowCount = 1;
+            this.tlpBase5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpBase5.Size = new System.Drawing.Size(448, 79);
+            this.tlpBase5.TabIndex = 4;
+            // 
+            // radNao
+            // 
+            this.radNao.AutoSize = true;
+            this.radNao.Location = new System.Drawing.Point(275, 3);
+            this.radNao.Name = "radNao";
+            this.radNao.Size = new System.Drawing.Size(45, 17);
+            this.radNao.TabIndex = 3;
+            this.radNao.TabStop = true;
+            this.radNao.Text = "Não";
+            this.radNao.UseVisualStyleBackColor = true;
+            // 
+            // rgbMensagem
+            // 
+            this.rgbMensagem.BackgroundColor = System.Drawing.Color.White;
+            this.rgbMensagem.BorderWidth = 1F;
+            this.rgbMensagem.CornerRadius = 10;
+            this.rgbMensagem.Location = new System.Drawing.Point(128, 308);
+            this.rgbMensagem.Name = "rgbMensagem";
+            this.rgbMensagem.Size = new System.Drawing.Size(448, 80);
+            this.rgbMensagem.TabIndex = 5;
+            this.rgbMensagem.TabStop = false;
+            // 
+            // rgbNomeP
+            // 
+            this.rgbNomeP.BackgroundColor = System.Drawing.Color.White;
+            this.rgbNomeP.BorderWidth = 1F;
+            this.rgbNomeP.CornerRadius = 10;
+            this.rgbNomeP.Location = new System.Drawing.Point(128, 26);
+            this.rgbNomeP.Name = "rgbNomeP";
+            this.rgbNomeP.Size = new System.Drawing.Size(448, 30);
+            this.rgbNomeP.TabIndex = 6;
+            this.rgbNomeP.TabStop = false;
+            // 
+            // rgbNomeAutores
+            // 
+            this.rgbNomeAutores.BackgroundColor = System.Drawing.Color.White;
+            this.rgbNomeAutores.BorderWidth = 1F;
+            this.rgbNomeAutores.CornerRadius = 10;
+            this.rgbNomeAutores.Location = new System.Drawing.Point(128, 85);
+            this.rgbNomeAutores.Name = "rgbNomeAutores";
+            this.rgbNomeAutores.Size = new System.Drawing.Size(448, 62);
+            this.rgbNomeAutores.TabIndex = 7;
+            this.rgbNomeAutores.TabStop = false;
+            // 
+            // rgbCompartilharCom
+            // 
+            this.rgbCompartilharCom.BackgroundColor = System.Drawing.Color.White;
+            this.rgbCompartilharCom.BorderWidth = 1F;
+            this.rgbCompartilharCom.CornerRadius = 10;
+            this.rgbCompartilharCom.Location = new System.Drawing.Point(128, 174);
+            this.rgbCompartilharCom.Name = "rgbCompartilharCom";
+            this.rgbCompartilharCom.Size = new System.Drawing.Size(448, 106);
+            this.rgbCompartilharCom.TabIndex = 8;
+            this.rgbCompartilharCom.TabStop = false;
+            // 
+            // rgbAcesso
+            // 
+            this.rgbAcesso.BackgroundColor = System.Drawing.Color.White;
+            this.rgbAcesso.BorderWidth = 1F;
+            this.rgbAcesso.CornerRadius = 10;
+            this.rgbAcesso.Location = new System.Drawing.Point(3, 25);
+            this.rgbAcesso.Name = "rgbAcesso";
+            this.rgbAcesso.Size = new System.Drawing.Size(572, 256);
+            this.rgbAcesso.TabIndex = 6;
+            this.rgbAcesso.TabStop = false;
+            // 
+            // frmGerenciamentoColaboradores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 598);
+            this.Controls.Add(this.tlpBase1);
+            this.Name = "frmGerenciamentoColaboradores";
+            this.Text = "frmGerenciamentoColaboradores";
+            this.tlpBase1.ResumeLayout(false);
+            this.tlpBase2.ResumeLayout(false);
+            this.tlpBase4.ResumeLayout(false);
+            this.tlpBase4.PerformLayout();
+            this.tlpBase3.ResumeLayout(false);
+            this.tlpBase3.PerformLayout();
+            this.tlpBase5.ResumeLayout(false);
+            this.tlpBase5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpBase1;
+        private System.Windows.Forms.TableLayoutPanel tlpBase2;
+        private System.Windows.Forms.TableLayoutPanel tlpBase4;
+        private System.Windows.Forms.Label lblAcesso;
+        private System.Windows.Forms.TableLayoutPanel tlpBase3;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label lblCompartilharC;
+        private System.Windows.Forms.Label lblNomeAutores;
+        private System.Windows.Forms.Label lblNomeP;
+        private System.Windows.Forms.TableLayoutPanel tlpBase5;
+        private System.Windows.Forms.Label lblCompartilhar;
+        private System.Windows.Forms.RadioButton radSim;
+        private System.Windows.Forms.RadioButton radNao;
+        private RoundedGroupBox rgbMensagem;
+        private RoundedGroupBox rgbAcesso;
+        private RoundedGroupBox rgbCompartilharCom;
+        private RoundedGroupBox rgbNomeAutores;
+        private RoundedGroupBox rgbNomeP;
+    }
+}
